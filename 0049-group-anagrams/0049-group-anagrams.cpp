fbfdad02f2c5453d8a745ac38f7e4263f837d6fe
Srivathsa05 <srivathsabhat096@gmail.com>
2026-08-25 22:26:58 +0530

    Time: 8 ms (96.34%), Space: 24.9 MB (81.74%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,47 +1,19 @@
 class Solution {
-private:
-    bool isAnagram(const string& r, const string& s) {
-        if (r.size() != s.size())
-            return false;
-
-        vector<int> hash(26, 0);
-
-        for (char c : r)
-            hash[c - 'a']++;
-
-        for (char c : s)
-            hash[c - 'a']--;
-
-        for (int n : hash) {
-            if (n != 0)
-                return false;
-        }
-
-        return true;
-    }
-
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();
+        unordered_map<string, vector<string>> mp;
+
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+
+            mp[key].push_back(s);
+        }
+
         vector<vector<string>> ans;
-        vector<bool> used(n, false);
 
-        for (int i = 0; i < n; i++) {
-            if (used[i])
-                continue;
-
-            vector<string> group;
-            group.push_back(strs[i]);
-            used[i] = true;
-
-            for (int j = i + 1; j < n; j++) {
-                if (!used[j] && isAnagram(strs[i], strs[j])) {
-                    group.push_back(strs[j]);
-                    used[j] = true;
-                }
-            }
-
-            ans.push_back(group);
+        for (auto& pair : mp) {
+            ans.push_back(pair.second);
         }
 
         return ans;
